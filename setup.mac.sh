11638a6e1fb91b43223197a65b96c908c91c61a0
Phong Nguyen <nhphong1406@gmail.com>
2016-12-10 16:38:15 +0700

    Refers to new repo URL

--- setup.mac.sh
+++ setup.mac.sh
@@ -18,7 +18,7 @@ MY_KEYMAP="$HOME/src/qmk_firmware/keyboards/ergodox/keymaps/$(whoami)"
 if [ -d "$MY_KEYMAP" ]; then
     cd "$MY_KEYMAP" ; git fetch -p ; git reset --hard origin/HEAD
 else
-    git clone https://github.com/phongnh/ergodox-ez "$MY_KEYMAP"
+    git clone https://github.com/phongnh/ergodox-ez-settings "$MY_KEYMAP"
 fi
 
 cd ~/src/qmk_firmware/keyboards/ergodox
