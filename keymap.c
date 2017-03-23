3c9598ba60c8c83fdbe72e23ebf2791d2eb768b8
Phong Nguyen <phong@Hummingbird.local>
2017-03-23 11:02:33 +0700

    Places [ and ] where I can easily touch them

--- keymap.c
+++ keymap.c
@@ -23,7 +23,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* My Custom Layer
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |    =   |   1  |   2  |   3  |   4  |   5  | Left |           | Right|   6  |   7  |   8  |   9  |   0  |   -    |
+ * |    =   |   1  |   2  |   3  |   4  |   5  |  [   |           |   ]  |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |  Del   |   Q  |   W  |   E  |   R  |   T  | ~L2  |           | ~L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_CUSTOM] = KEYMAP(
     // left hand
-    KC_EQL,               KC_1,             KC_2,           KC_3,     KC_4,     KC_5,           KC_LEFT,
+    KC_EQL,               KC_1,             KC_2,           KC_3,     KC_4,     KC_5,           KC_LBRC,
     KC_DELT,              KC_Q,             KC_W,           KC_E,     KC_R,     KC_T,           KC_FN2,
     ALT_T(KC_BSPC),       KC_A,             KC_S,           KC_D,     KC_F,     KC_G,
     KC_LSFT,              CTL_T(KC_Z),      KC_X,           KC_C,     KC_V,     KC_B,           ALL_T(KC_ESC),
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                 KC_HOME,
                                                                       KC_SPC,   KC_BSPC,        KC_END,
     // right hand
-    KC_RGHT,           KC_6,            KC_7,         KC_8,     KC_9,     KC_0,                 KC_MINS,
+    KC_RBRC,           KC_6,            KC_7,         KC_8,     KC_9,     KC_0,                 KC_MINS,
     KC_FN1,            KC_Y,            KC_U,         KC_I,     KC_O,     KC_P,                 KC_BSLS,
                        KC_H,            KC_J,         KC_K,     KC_L,     LT(_MEDIA, KC_SCLN),  GUI_T(KC_QUOT),
     MEH_T(KC_SPC),     KC_N,            KC_M,         KC_COMM,  KC_DOT,   CTL_T(KC_SLSH),       KC_RSFT,
