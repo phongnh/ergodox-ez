9abc8e16d40718993102304e11afcd521f518a66
Phong Nguyen <phong@Hummingbird.local>
2017-03-30 17:04:42 +0700

    Updates mappings

--- keymap.c
+++ keymap.c
@@ -23,15 +23,15 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* My Custom Layer
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |    =   |   1  |   2  |   3  |   4  |   5  |  [   |           |   ]  |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |    =   |   1  |   2  |   3  |   4  |   5  | Del  |           | Del  |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |  Del   |   Q  |   W  |   E  |   R  |   T  | ~L2  |           | ~L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |  Tab   |   Q  |   W  |   E  |   R  |   T  | ~L2  |           | ~L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |Alt/Bks |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / LGui|
+ * |Alt/Esc |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / LGui|
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * |LShift  |Z/Ctrl|   X  |   C  |   V  |   B  | Tab  |           |Space |   N  |   M  |   ,  |   .  |//Ctrl|RShift  |
+ * |LShift  |Z/Ctrl|   X  |   C  |   V  |   B  |  [   |           |  ]   |   N  |   M  |   ,  |   .  |//Ctrl|RShift  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|'"/CAG|AltShf|   [  |   ]  |                                       | Left | Down |  Up  |Right | ~L1  |
+ *   |Grv/L1|'"/CAG|AltShf|   [  |]/LGui|                                       | Left | Down |  Up  |Right | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                       ,--------------.       ,---------------.
  *                                       |App/Alt| LGui |       | Alt  |Ctrl/Esc|
@@ -43,19 +43,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_CUSTOM] = KEYMAP(
     // left hand
-    KC_EQL,               KC_1,             KC_2,           KC_3,     KC_4,     KC_5,           KC_LBRC,
-    KC_DELT,              KC_Q,             KC_W,           KC_E,     KC_R,     KC_T,           KC_FN2,
-    ALT_T(KC_BSPC),       KC_A,             KC_S,           KC_D,     KC_F,     KC_G,
-    KC_LSFT,              CTL_T(KC_Z),      KC_X,           KC_C,     KC_V,     KC_B,           ALL_T(KC_TAB),
-    LT(_SYMBOL, KC_GRV),  LCAG_T(KC_QUOT),  LALT(KC_LSFT),  KC_LBRC,  KC_RBRC,
-                                                                                ALT_T(KC_APP),  KC_LGUI,
-                                                                                                KC_HOME,
-                                                                      KC_SPC,   KC_BSPC,        KC_END,
+    KC_EQL,               KC_1,             KC_2,           KC_3,     KC_4,            KC_5,           KC_DELT,
+    KC_TAB,               KC_Q,             KC_W,           KC_E,     KC_R,            KC_T,           KC_FN2,
+    ALT_T(KC_ESC),        KC_A,             KC_S,           KC_D,     KC_F,            KC_G,
+    KC_LSFT,              CTL_T(KC_Z),      KC_X,           KC_C,     KC_V,            KC_B,           ALL_T(KC_LBRC),
+    LT(_SYMBOL, KC_GRV),  LCAG_T(KC_QUOT),  LALT(KC_LSFT),  KC_LBRC,  GUI_T(KC_RBRC),
+                                                                                       ALT_T(KC_APP),  KC_LGUI,
+                                                                                                       KC_HOME,
+                                                                      KC_SPC,          KC_BSPC,        KC_END,
     // right hand
-    KC_RBRC,           KC_6,            KC_7,         KC_8,     KC_9,     KC_0,                 KC_MINS,
+    KC_DELT,           KC_6,            KC_7,         KC_8,     KC_9,     KC_0,                 KC_MINS,
     KC_FN1,            KC_Y,            KC_U,         KC_I,     KC_O,     KC_P,                 KC_BSLS,
                        KC_H,            KC_J,         KC_K,     KC_L,     LT(_MEDIA, KC_SCLN),  GUI_T(KC_QUOT),
-    MEH_T(KC_SPC),     KC_N,            KC_M,         KC_COMM,  KC_DOT,   CTL_T(KC_SLSH),       KC_RSFT,
+    MEH_T(KC_RBRC),    KC_N,            KC_M,         KC_COMM,  KC_DOT,   CTL_T(KC_SLSH),       KC_RSFT,
                                         KC_LEFT,      KC_DOWN,  KC_UP,    C_S_T(KC_RIGHT),      KC_FN1,
     KC_LALT,           CTL_T(KC_ESC),
     KC_PGUP,
