4a2547cdff3a52672fb966fedf20dffde7311277
Phong Nguyen <nhphong1406@gmail.com>
2018-01-05 19:23:19 +0700

    Removes unused config

--- config.h
+++ config.h
@@ -21,7 +21,7 @@
 // #define ONESHOT_TAP_TOGGLE 2
 // #define ONESHOT_TIMEOUT    3000
 
-#undef  LEADER_TIMEOUT
-#define LEADER_TIMEOUT 1000
+// #undef  LEADER_TIMEOUT
+// #define LEADER_TIMEOUT 1000
 
 #endif /* CONFIG_PHONGNH_H_ */
